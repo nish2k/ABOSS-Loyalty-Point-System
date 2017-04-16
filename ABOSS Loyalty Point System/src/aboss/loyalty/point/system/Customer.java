9c4f91fdd6b0a52df32d40215973d3b48e9dc44d
nish2k <nishvbrown@gmail.com>
2017-04-16 18:17:44 -0500

    Add files via upload
    QR Code library and test

--- ABOSS Loyalty Point System/src/aboss/loyalty/point/system/Customer.java
+++ ABOSS Loyalty Point System/src/aboss/loyalty/point/system/Customer.java
@@ -24,11 +24,11 @@ public class Customer {
        
     custList = new Customer[10];
     
-      custList[0] = new Customer ("12322","1234DES","Towers Hall, Uwi Mona","Bob Myers",0);
+      custList[0] = new Customer ("12322","1234DES","Towers Hall, Uwi Mona","Bob Myers",1232);
 
-      custList[1] = new Customer("12232","154!EES","Chancellor, Uwi Mona","Crippy Myers",0);
+      custList[1] = new Customer("12232","154!EES","Chancellor, Uwi Mona","Crippy Myers",1232);
       
-      custList[2] = new Customer("18322","1114SES","Towers Hall, Uwi Mona","Eastern Myers",0);   
+      custList[2] = new Customer("18322","1114SES","Towers Hall, Uwi Mona","Eastern Myers",1232);   
     }
     
     public Customer (String initialId, String uniqueId, String name, String address, int points){
@@ -36,8 +36,7 @@ public class Customer {
         this.uniqueId = uniqueId;
         this.address = address;
         this.name = name;
-        points = 0;
-        
+        this.points = points;
     }
     
     public void addCustomer(Customer n){
@@ -74,14 +73,13 @@ public class Customer {
     return address;
     }
     
-    public String viewCustomer(){
+    public String viewCustomer(Customer n){
     
-        String mess = "Customer name: " + getName();
-        mess+= "\n Customer initial ID: " + getinitialID();
-        mess+= "\nCustomer unique ID: " + getuniqueID();
-        mess+= "\n Customer address: " + getAddress();
-        mess+= "\nCurrent customer points: " + getPoints();
-        mess+= "\n_________________________________________________________";
+        String mess = "Customer name: " + n.getName();
+        mess+= "\n Customer initial ID: " + n.getinitialID();
+        mess+= "\nCustomer unique ID: " + n.getuniqueID();
+        mess+= "\n Customer address: " + n.getAddress();
+        mess+= "\nCurrent customer points: " +n.getPoints();
         
         return mess;   
     }
@@ -89,7 +87,7 @@ public class Customer {
    public Customer returnCus(String y){
    int x = 0;
    int u = 0;
-   Customer t = new Customer("","","","");
+   Customer t = new Customer("","","","",0);
    System.out.println(custList.length);
    while (x<custList.length){
         System.out.println(custList[x].getuniqueID());
